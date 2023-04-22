b7d9d1836aa6600bd6d809ab83aaa719685d483f
michaelmoonlvr <migueljomfv@gmail.com>
2023-04-22 12:03:44 -0500

    Implementaciob de

--- src/main/java/pe/edu/pe/onlinebooks/controllers/BookController.java
+++ src/main/java/pe/edu/pe/onlinebooks/controllers/BookController.java
@@ -55,5 +55,27 @@ public class BookController {
             return ResponseEntity.ok(user);
         }
 
+        @RestController
+        @RequestMapping("/api/favorites")
+        public class FavoriteBookController {
+            private final FavoriteBookService favoriteBookService;
+            public FavoriteBookController(FavoriteBookService favoriteBookService) {
+                this.favoriteBookService = favoriteBookService;
+            }
+            @PostMapping
+            public ResponseEntity<FavoriteBook> addFavoriteBook(@RequestParam Long
+                                                                        bookId, @RequestParam Long userId) {
+                FavoriteBook newFavoriteBook =
+                        favoriteBookService.addFavoriteBook(bookId, userId);
+                return ResponseEntity.ok(newFavoriteBook);
+            }
+            @GetMapping("/{userId}")
+            public ResponseEntity<List<Book>>
+            getFavoriteBooksByUserId(@PathVariable Long userId) {
+                List<Book> favoriteBooks =
+                        favoriteBookService.getFavoriteBooksByUserId(userId);
+                return ResponseEntity.ok(favoriteBooks);
+            }
+
 }
 
